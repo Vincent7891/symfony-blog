aafee1bb0a34ea781d03c21b2cc0b63ba5937acb
Vincent Dejean <vincent.dejean@akeneo.com>
2025-01-06 09:34:41 +0000

    fix: re-add post validation message

--- back/src/Domain/Model/PostContent.php
+++ back/src/Domain/Model/PostContent.php
@@ -1,21 +1,26 @@
 <?php
 
 namespace App\Domain\Model;
-
 use App\Domain\Exception\InvalidPostException;
 
 class PostContent
 {
     public function __construct(
-        public readonly string $post,
+        private readonly string $post,
     ){
-        if (empty($post)) {
+        $this->validatePost($post);
+    }
+
+    private function validatePost(string $post): void
+    {
+        if(empty($post)) {
             throw new InvalidPostException(
                 'Content cannot be empty'
             );
         }
     }
-    public function getPost(): string {
+    public function getPost(): string
+    {
         return $this->post;
     }
 }
