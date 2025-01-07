102f86ffeb73a276c30861d3194ec80366008731
Vincent Dejean <vincent.dejean@akeneo.com>
2025-01-07 14:29:40 +0000

    lint

--- back/src/Infrastructure/Controller/DeletePostController.php
+++ back/src/Infrastructure/Controller/DeletePostController.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Infrastructure\Controller;
 
 use App\Application\DeletePost\DeletePostCommand;
 use App\Application\DeletePost\DeletePostHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DeletePostController
+class DeletePostController extends AbstractController
 {
     public function __construct(
         private readonly DeletePostHandler $handler,
