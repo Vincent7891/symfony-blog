16fc90c4c22364b1a19a30cb47d68173e5fe1dc8
Vincent Dejean <vincent.dejean@akeneo.com>
2025-01-09 11:41:07 +0000

    add linting packages, basic styling

--- front/src/components/Pages/EditPostPage.tsx
+++ front/src/components/Pages/EditPostPage.tsx
@@ -1,51 +1,57 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
-import {editPost, getPosts, Post} from "../../api/api.ts";
-import PostForm from "../Organisms/PostForm.tsx";
+import { useNavigate, useParams } from 'react-router-dom'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { editPost, getPosts, Post } from '../../api/api.ts'
+import PostForm from '../Organisms/PostForm.tsx'
 
 const EditPostPage = () => {
-    const {id} = useParams();
-    const navigate = useNavigate();
-    const queryClient = useQueryClient();
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
 
-    const {data:posts, isLoading, isError} = useQuery<Post[]>({
-        queryKey:["posts"],
-        queryFn: getPosts,
-    })
+  const {
+    data: posts,
+    isLoading,
+    isError,
+  } = useQuery<Post[]>({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+  })
 
-    const editMutation = useMutation({
-        mutationFn: (post: Post)=> editPost(post),
-        onSuccess: ()=>{
-            queryClient.invalidateQueries({queryKey: ["posts"]});
-            navigate("/posts")
-        }
-    })
+  const editMutation = useMutation({
+    mutationFn: (post: Post) => editPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+      navigate('/posts')
+    },
+  })
 
-    if(isLoading) return <p>loading ...</p>
-    if(isError) return <p>error loading posts</p>;
-    const postToEdit = posts?.find(post => post.id === Number(id));
-    if(!postToEdit) return <p>post not found</p>;
+  if (isLoading) return <p>loading ...</p>
+  if (isError) return <p>error loading posts</p>
+  const postToEdit = posts?.find((post) => post.id === Number(id))
+  if (!postToEdit) return <p>post not found</p>
 
-    const handleEdit = (formData: Post) => {
-        if(postToEdit.id){
-            const payload = {
-                id: formData.id,
-                title: formData.title,
-                content: formData.content,
-            }
-            console.log(payload)
-            editMutation.mutate(payload);
-        }
+  const handleEdit = (formData: Post) => {
+    if (postToEdit.id) {
+      const payload = {
+        id: formData.id,
+        title: formData.title,
+        content: formData.content,
+      }
+      console.log(payload)
+      editMutation.mutate(payload)
     }
+  }
 
-    return(
-        <div>
-            <h1>Edit Post</h1>
-            <PostForm initialData={postToEdit}
-                      onSubmit={handleEdit}
-                      submitText={'Edit Post'}/>
-        </div>
-    )
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center bg-gray-100 p-4">
+      <h1 className="font-semibold text-2xl text-center my-4">Edit Post</h1>
+      <PostForm
+        initialData={postToEdit}
+        onSubmit={handleEdit}
+        submitText={'Edit Post'}
+      />
+    </div>
+  )
 }
 
-export default EditPostPage;
+export default EditPostPage
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
